2513b4accebdb864e33f1c43f61844aefa684e42
Sabry Awad <dr.sabry97@gmail.com>
2024-01-14 13:17:48 +0200

    🎉 feat(package.json): Rename project to "bun-pwned-passwords" and add "commander" dependency ✨ feat(index.test.ts): Add test for password hashing in PasswordChecker class 💡 chore(index.ts): Add PasswordChecker class with hashPassword method

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,19 @@
-import { describe, expect, test } from "bun:test";
+import { describe, expect, it } from "bun:test";
 
-describe("Hello World", () => {
-  test("works", () => {
-    expect(1 + 1).toBe(2);
+import { PasswordChecker, type PasswordCheckResult } from ".";
+
+describe("PasswordChecker", () => {
+  describe("hashPassword", () => {
+    it("should correctly hash a password using SHA-1", () => {
+      // Arrange
+      const password = "password";
+
+      // Act
+      const hashedPassword = PasswordChecker.hashPassword(password);
+      const expectedHash = "5BAA61E4C9B93F3F0682250B6CF8331B7EE68FD8";
+
+      // Assert
+      expect(hashedPassword).toBe(expectedHash);
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+export interface PasswordCheckResult {
+  password: string;
+  status: "Compromised" | "Safe";
+  score?: number;
+}
+
+export class PasswordChecker {
+  public static hashPassword(password: string): string {
+    const hash = new Bun.CryptoHasher("sha1");
+    hash.update(password);
+    return hash.digest("hex").toUpperCase();
+  }
+}
